c4cde3c178232c5df00324b1517600d0ecf17c18
AlexanderrAviles <aviles.alex321@gmail.com>
2023-09-01 14:46:09 -0400

    third commit

--- src/Noticias.js
+++ src/Noticias.js
@@ -4,6 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Noticias() {
   const [noticias, setNoticias] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredNoticias, setFilteredNoticias] = useState([]);
 
   useEffect(() => {
     const apiKey = "key4Ft3bPETfNsC3J";
@@ -17,7 +19,6 @@ function Noticias() {
       })
       .then((response) => {
         // Extrae los datos que necesitas de la respuesta
-        console.log(response.data.records);
         const data = response.data.records.map((record) => ({
           id: record.id,
           fecha_publicacion: record.fields.fecha_publicacion,
@@ -28,6 +29,9 @@ function Noticias() {
           estado: record.fields.estado,
         }));
 
+        // Inicializa las noticias filtradas con todas las noticias
+        setFilteredNoticias(data);
+
         // Actualiza el estado del componente con los datos de Airtable
         setNoticias(data);
       })
@@ -36,29 +40,67 @@ function Noticias() {
       });
   }, []);
 
+  const handleSearch = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+
+    // Filtra las noticias basadas en el término de búsqueda
+    const filtered = noticias.filter((noticia) => {
+      return (
+        noticia.titular.toLowerCase().includes(value.toLowerCase()) ||
+        (noticia.contenido &&
+          noticia.contenido.toLowerCase().includes(value.toLowerCase()))
+      );
+    });
+
+    // Actualiza el estado de las noticias filtradas
+    setFilteredNoticias(filtered);
+  };
+
   return (
     <div className="container">
-      <h1 class="text-center">Noticias</h1>
-      <div class="row">
-        {noticias.map((noticia) => (
-          <div key={noticia.id} class="card mb-2 me-2" style={{width: 25 + 'rem', height: 40+'rem'}}>
-              <img
-                src={noticia.galeria}
-                alt="Imagen de noticia" class="card-img-top"
-                style={{ maxWidth: '398px', maxHeight: '200px', objectFit: 'cover' }}
-
-              />
-              <div class="card-body">
-              <h5  class="card-title">{noticia.titular}</h5>
-              <p><strong>Fecha de publicación:</strong> {noticia.fecha_publicacion}</p>
-              <p >{noticia.autor}</p>
-              {noticia.contenido && (
-              <p class="card-text">
-                {noticia.contenido.substring(0, 300)}
+      <h1 className="text-center">Noticias</h1>
+        <label for="buscador" className="form-label">Buscar Noticia:</label>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearch}
+        id="buscador"
+        className="form-control mb-3"
+      />
+      <div className="row">
+        {filteredNoticias.map((noticia) => (
+          <div
+            key={noticia.id}
+            className="card mb-2 me-2"
+            style={{ width: "25rem", height: "40rem" }}
+          >
+            <img
+              src={noticia.galeria}
+              alt="Imagen de noticia"
+              className="card-img-top"
+              style={{
+                maxWidth: "398px",
+                minWidth: "300px",
+                minHeight: "200px",
+                maxHeight: "200px",
+                objectFit: "cover",
+              }}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{noticia.titular}</h5>
+              <p>
+                <strong>Fecha de publicación:</strong>{" "}
+                {noticia.fecha_publicacion}
               </p>
-            )}
+              <p>{noticia.autor}</p>
+              {noticia.contenido && (
+                <p className="card-text">
+                  {noticia.contenido.substring(0, 300)}
+                </p>
+              )}
             </div>
-            <br></br>
+            <br />
           </div>
         ))}
       </div>
